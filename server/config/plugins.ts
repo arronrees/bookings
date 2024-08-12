0fe80edc242309a22f3c5c7b6580dbbb808bd41a
arronrees <64908445+arronrees@users.noreply.github.com>
2024-08-12 20:14:29 +0100

    Added smtp plugin

--- server/config/plugins.ts
+++ server/config/plugins.ts
@@ -1,1 +1,16 @@
-export default () => ({});
+export default ({ env }) => ({
+  email: {
+    config: {
+      provider: "amazon-ses",
+      providerOptions: {
+        key: env("AWS_SES_KEY"),
+        secret: env("AWS_SES_SECRET"),
+        amazon: "https://email-smtp.eu-west-2.amazonaws.com",
+      },
+      settings: {
+        defaultFrom: env("SMTP_DEFAULT_FROM"),
+        defaultReplyTo: env("SMTP_DEFAULT_FROM"),
+      },
+    },
+  },
+});
